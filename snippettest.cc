633afaac872d67d2ab7188b93bdc59be7fdb259b
Mihai Bivol <mm.bivol@gmail.com>
2012-06-29 03:55:51 +0300

    Make snippettest run on ground_truth_generator generated files

--- snippettest.cc
+++ snippettest.cc
@@ -23,67 +23,39 @@
 #include <xapian.h>
 
 #include <iostream>
+#include <fstream>
 #include <string>
 
 #include <cstdlib> // For exit().
 #include <cstring>
 
+#define QUERY_LEN 1024
+
 using namespace std;
 
-int
-main(int argc, char **argv)
-try {
-    // We require at least two command line arguments.
-    if (argc < 3) {
-	int rc = 1;
-	if (argv[1]) {
-	    if (strcmp(argv[1], "--version") == 0) {
-		cout << "simplesnippetgen" << endl;
-		exit(0);
-	    }
-	    if (strcmp(argv[1], "--help") == 0) {
-		rc = 0;
-	    }
-	}
-	cout << "Usage: " << argv[0] << " PATH_TO_DATABASE QUERY" << endl;
-	exit(rc);
-    }
+void
+test_file(
+	const char *		    filename,
+	Xapian::QueryParser &	    qp,
+	Xapian::Enquire &	    enquire)
+{
+    ifstream file(filename);
+    char query_s[QUERY_LEN];
+    file.getline(query_s, QUERY_LEN);
 
-    // Open the database for searching.
-    Xapian::Database db(argv[1]);
-
-    // Start an enquire session.
-    Xapian::Enquire enquire(db);
-
-    // Combine the rest of the command line arguments with spaces between
-    // them, so that simple queries don't have to be quoted at the shell
-    // level.
-    string query_string(argv[2]);
-    argv += 3;
-    while (*argv) {
-	query_string += ' ';
-	query_string += *argv++;
-    }
-
-    // Parse the query string to produce a Xapian::Query object.
-    Xapian::QueryParser qp;
-    Xapian::Stem stemmer("english");
-    qp.set_stemmer(stemmer);
-    qp.set_database(db);
-    qp.set_stemming_strategy(Xapian::QueryParser::STEM_SOME);
-    Xapian::Query query = qp.parse_query(query_string);
-    cout << "Parsed query is: " << query.get_description() << endl;
-
-    // Find the top 10 results for the query.
+    cout << query_s << "\n";
+    Xapian::Query query = qp.parse_query(string(query_s));
     enquire.set_query(query);
     Xapian::MSet matches = enquire.get_mset(0, 10);
+
+    Xapian::Snipper snipper;
+    Xapian::Stem stemmer("english");
+    // Find the top 10 results for the query.
 
     // Display the results.
     cout << matches.get_matches_estimated() << " results found.\n";
     cout << "Matches 1-" << matches.size() << ":\n" << endl;
 
-    Xapian::Snipper snipper;
-    snipper.set_stemmer(stemmer);
     for (Xapian::MSetIterator i = matches.begin(); i != matches.end(); ++i) {
 	// Get only the sample from the document data.
 	string gen_text = i.get_document().get_data();
@@ -98,6 +70,43 @@ try {
 
 	cout << i.get_rank() + 1 << ": " << i.get_weight() << " docid=" << *i
 	     << " [" << snipper.generate_snippet(matches, gen_text) << "]\n\n";
+    }
+   snipper.set_stemmer(stemmer);
+}
+
+int
+main(int argc, char **argv)
+try {
+    // We require at least two command line arguments.
+    if (argc < 3) {
+	int rc = 1;
+	if (argv[1]) {
+	    if (strcmp(argv[1], "--version") == 0) {
+		cout << "snippettest" << endl;
+		exit(0);
+	    }
+	    if (strcmp(argv[1], "--help") == 0) {
+		rc = 0;
+	    }
+	}
+	cout << "Usage: " << argv[0] << " PATH_TO_DATABASE FILES" << endl;
+	exit(rc);
+    }
+
+    // Open the database for searching.
+    Xapian::Database db(argv[1]);
+
+    // Start an enquire session.
+    Xapian::Enquire enquire(db);
+
+    // Set query parser.
+    Xapian::QueryParser qp;
+    Xapian::Stem stemmer("english");
+    qp.set_stemmer(stemmer);
+    qp.set_database(db);
+    qp.set_stemming_strategy(Xapian::QueryParser::STEM_SOME);
+    for (int i = 2; i < argc; i++) {
+	test_file(argv[i], qp, enquire);
     }
 } catch (const Xapian::Error &e) {
     cout << e.get_description() << endl;
